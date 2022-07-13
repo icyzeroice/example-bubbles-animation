c198b12a24ed7d403d675d4aac593994bc164aef
icyzeroice <ziv3@outlook.com>
2022-07-13 13:18:35 +0800

    feat: make the example of paperjs candy crash

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,13 +1,33 @@
-import "./style.css";
+import "./style.css"
+import { paper } from "./context"
+import { Ball, view } from "./ball"
 
-// function main(canvas: HTMLCanvasElement) {
-//   function render() {
-//     onFrame()
-//     requestAnimationFrame(render)
-//   }
+const balls: Ball[] = []
+const numBalls = 18
 
-//   onStart(canvas)
-//   render()
-// }
+for (var i = 0; i < numBalls; i++) {
+  const position = paper.Point.random().multiply(view.size)
 
-// main(document.querySelector<HTMLCanvasElement>('#canvas')!)
+  const velocity = new paper.Point({
+    angle: 360 * Math.random(),
+    length: Math.random() * 10,
+  })
+
+  const radius = Math.random() * 30 + 30
+
+  balls.push(new Ball(radius, position, velocity))
+}
+
+;(function onFrame() {
+  for (var i = 0; i < balls.length - 1; i++) {
+    for (var j = i + 1; j < balls.length; j++) {
+      balls[i].react(balls[j])
+    }
+  }
+
+  for (var i = 0, l = balls.length; i < l; i++) {
+    balls[i].iterate()
+  }
+
+  requestAnimationFrame(onFrame)
+})()
