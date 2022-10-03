bc6c76ab41800530cf81769b5bf0c909f1044955
xiaowei <xiaowei305@163.com>
2022-10-03 16:17:59 +0800

    feat: setup camera code

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 
-const ViteConfigForAppVallina = defineConfig({});
+const ViteConfigForAppVallina = defineConfig({
+  server: {
+    proxy: {
+      '/camera': {
+        target: 'ws://127.0.0.1:8000/camera',
+        ws: true
+      }
+    }
+  }
+});
 
 // `defineConfig` can mention you with the type system
 export default async ({ command, config }) => {
